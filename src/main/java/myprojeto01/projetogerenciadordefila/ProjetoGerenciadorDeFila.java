2cd0758a6cc0fdf7497c9927af03a50c327d9164
Vitor Arthur Schuck <vitor08arthur@gmail.com>
2024-05-14 00:23:25 -0300

    Commit incial

---
+++ src/main/java/myprojeto01/projetogerenciadordefila/ProjetoGerenciadorDeFila.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package myprojeto01.projetogerenciadordefila;
+
+/**
+ *
+ * @author Vitor
+ */
+public class ProjetoGerenciadorDeFila {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
